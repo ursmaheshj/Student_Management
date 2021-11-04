81f0ce731378eb0c8d4b78bdfa02d0070ecc819a
Mahesh Jadhav <omkar28399@gmail.com>
2021-11-04 10:59:42 +0530

    views updated

--- Main_App/views.py
+++ Main_App/views.py
@@ -1,3 +1,43 @@
+from django.contrib.auth import authenticate,login,logout
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+from django.contrib import messages
+from Main_App import EmailAuthentication
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def loginpage(request):
+    return render(request,'loginpage.html')
+
+def loginuser(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method not allowed</h2>")
+    else : 
+        user = EmailAuthentication.authenticate(request,username=request.POST.get('email'),password=request.POST.get('password'))
+        if user!=None:
+            login(request,user)
+            if user.user_type=='1':
+                return HttpResponseRedirect('/adminhome')
+            elif user.user_type=='2':
+                return HttpResponseRedirect('/teacherhome')
+            elif user.user_type=='3' :
+                return HttpResponseRedirect('/studenthome')
+            else :
+                return HttpResponse("Invalid user type")
+        else :
+            messages.error(request,"Invalid login details..!")
+            return HttpResponseRedirect("/")
+
+def userprofile(request):
+    if request.user != None:
+        return HttpResponse("User : "+request.user.email+" UserType"+request.user.user_type )
+    else :
+        return HttpResponse("Please login first")
+
+def logoutuser(request):
+    logout(request)
+    return HttpResponseRedirect('/index')
