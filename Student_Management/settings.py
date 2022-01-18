509b1d2889f4c34e8794bd4f8ce90619883d1908
Mahesh Jadhav <omkar28399@gmail.com>
2022-01-19 01:17:58 +0530

    whitenoise dependency added

--- Student_Management/settings.py
+++ Student_Management/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +135,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Auth User Model
 AUTH_USER_MODEL = "Main_App.MyUser"
