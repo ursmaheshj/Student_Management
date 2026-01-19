0e94d48e6a78edb599e8531ae694d9c845de73c9
Akhil-Paladug <akhilpaladugu499@gmail.com>
2026-01-19 17:27:52 +0530

    Messed up the code

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Student_Management.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as ex:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
